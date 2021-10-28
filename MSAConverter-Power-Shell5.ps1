dd48b104056da9048b669e1e4adc93e1fb79bf05
Old Guy Geek <93287158+OldGuyGeek@users.noreply.github.com>
2021-10-28 14:08:48 -0700

    Added Open Destination Directory for Zips Files

---
+++ MSAConverter-Power-Shell5.ps1
@@ -0,0 +1,77 @@
+<# NOTES
+1. Does not gracefully exit if file selection is cancelled
+2. Does not check validity of .zip files
+3. Does not restrict output directory name to Atari 8 Character Limit
+4. Does not extract .stx (MSA limitation)
+#>
+
+
+# Create System File Open Diaglogue Box
+Add-Type -AssemblyName System.Windows.Forms
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Title = "Please Select File"
+$OpenFileDialog.InitialDirectory = $initialDirectory
+
+# Create filter for allowable file types
+$OpenFileDialog.filter = "ST Disk|*.st|STX Disk|*.stx|MSA Disk|*.MSA|Zips|*.zip"
+
+
+# Out-Null supresses the "OK" after selecting the file.
+$OpenFileDialog.ShowDialog() | Out-Null
+$Global:SelectedFile = $OpenFileDialog.FileName
+
+# Put the name of the selected file in a variable
+Set-Variable -Name "msafile" -Value $Global:SelectedFile
+Get-Variable msafile -ValueOnly
+
+#Create object to hold all the needed variables Easier for later use
+$msainput = New-Object psobject -Property @{
+    NeededQuotes      = """"
+    SingleQuotes      = "'"
+    # MsaPrgLoc = "H:\Atari\Window Stuff\MSA Converter 2.1\msa.exe"
+    ConvertType       = "diskimg_to_hdisk"
+    DestinationFolder = "H:\Atari\16bit\Emulators\D Drive\Extracts"
+}
+
+# To get destination subfolder name based on the name of the file selected
+$SubFolderName = [System.IO.Path]::GetFileNameWithoutExtension($Global:SelectedFile)
+$FileExtensionType = [System.IO.Path]::GetExtension($Global:SelectedFile)
+# Going to the Destination folder in order to create new directory
+Set-Location -Path $msainput.DestinationFolder
+
+
+# Create new directory based on selected file
+# NOTE! NO CHECKING IF DIRECTORY ALREADY EXISTS #>
+New-Item -Path $SubFolderName -ItemType Directory
+
+# Contatating names into final destination 
+$FullDestinationFolder = $msainput.DestinationFolder + "\" + $SubFolderName
+
+
+# If it was a zip flie, extract .zip to new directory
+
+if ($FileExtensionType -eq ".ZIP") { 
+    # extract Zip file to directory
+    Expand-Archive -LiteralPath $Global:SelectedFile -DestinationPath $FullDestinationFolder
+   Invoke-Item .
+}
+
+else {
+
+    # Going to MSA location to invoke it
+    Set-Location -Path "H:\Atari\Window Stuff\MSA Converter 2.1\"
+
+    # Starting MSA with command line parameters
+    Start-Process .\msa.exe "$($msainput.NeededQuotes)$($msainput.ConvertType)$($msainput.NeededQuotes) $($msainput.NeededQuotes)$msafile$($msainput.NeededQuotes) $($msainput.NeededQuotes)$($FullDestinationFolder)$($msainput.NeededQuotes)"
+
+    # Lazy way to make sure files are finished writing before closing MSA
+    Start-Sleep -Seconds 10
+    # Closing MSA
+    Stop-Process -processname msa
+    Invoke-Item -path $msainput.DestinationFolder
+}
+
+
+
+
+
